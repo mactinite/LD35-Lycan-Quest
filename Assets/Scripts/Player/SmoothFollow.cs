3b9144ca77846545b8309e4fecef9ac780f660ce
Hunter MacDonald <Hunter MacDonald>
2016-04-15 21:49:11 -0700

    got the basic movement added and the world mocked with a placeholder tileset

---
+++ Assets/Scripts/Player/SmoothFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour {
+
+	public float dampening = 25.0f;
+	public Transform target;
+
+	private Vector3 targetPos;
+	// Use this for initialization
+	void Start () {
+		targetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		targetPos.x = Mathf.Lerp (targetPos.x, target.position.x, Time.deltaTime * dampening);
+		targetPos.y = Mathf.Lerp (targetPos.y, target.position.y, Time.deltaTime * dampening);
+
+		transform.position = targetPos;
+
+	}
+}
